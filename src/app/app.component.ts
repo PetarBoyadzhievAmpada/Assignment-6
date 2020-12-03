e7ead295d8f8fef6e90754344cfc16658bd96483
PetarBoyadzhievAmpada <petar.boyadzhiev@ampada.local>
2020-12-03 10:32:59 +0100

    Cosmetic changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,24 +7,18 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  @ViewChild('f') signupForm:NgForm;
+  @ViewChild('f') signupForm: NgForm;
   title = 'assignment6';
-  defaultSubscription = 'Advanced';
-  submitted = false;
-  onSubmit(form: NgForm) {
-    this.submitted = true;
-    console.log(form);
-    this.user.email = this.signupForm.value.email;
-    this.user.subscription = this.signupForm.value.Subscription;
-    this.user.password = this.signupForm.value.password;
-
-  }
 
   user = {
     email: '',
     subscription: '',
-    password: ''
+    password: '',
+  };
+
+  onSubmit(form: NgForm): void {
+    this.user.email = this.signupForm.value.email;
+    this.user.subscription = this.signupForm.value.Subscription;
+    this.user.password = this.signupForm.value.password;
   }
-
-
 }
